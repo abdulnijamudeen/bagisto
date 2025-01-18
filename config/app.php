8244bef17a755df4fa9d7d4704bbb755e47f77e6
Nizam Abdul <abdulnijamudeen@gmail.com>
2025-01-18 11:15:46 +0530

    added app url

--- config/app.php
+++ config/app.php
@@ -66,7 +66,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://test.appstoo.in'),
 
     /*
     |--------------------------------------------------------------------------
